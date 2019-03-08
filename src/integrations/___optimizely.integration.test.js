9f0dccda1e7e67aaff461be81ca51d39da908ffe
Jop de Klein <jop@validweb.nl>
2019-03-08 16:34:24 +0100

    Remove outdated tests, add relevant audience integration tests

--- src/integrations/___optimizely.integration.test.js
+++ src/integrations/___optimizely.integration.test.js
@@ -34,59 +34,78 @@ optimizelyClientInstance.notificationCenter.addNotificationListener(
 
 const fooUserIdFalse = 'zhhhh' // control
 const fooUserIdTrue = 'barbazhhhaaah' // variation
+const barUserIdFalse = 'w=zkhkjz' // control
+const barUserIdTrue = '2987010978kjhkjhd82' // variation
 
-const attributes = {
+const fooCorrectSegmentationAttributes = {
   trafficSource: 'foo',
   deviceType: 'desktop',
   hasDefaultDates: true
 }
 
+const fooIncorrectSegmentationAttributes = {
+  trafficSource: 'foo',
+  deviceType: 'desktop',
+  hasDefaultDates: false
+}
+
 it('returns the correct result with experiment / variables combination', () => {
   expect(
-    optimizelyClientInstance.isFeatureEnabled('foo', fooUserIdTrue, attributes)
+    optimizelyClientInstance.isFeatureEnabled(
+      'foo',
+      fooUserIdTrue,
+      fooCorrectSegmentationAttributes
+    )
   ).toEqual(true)
+
   expect(
-    optimizelyClientInstance.isFeatureEnabled('foo', fooUserIdFalse, attributes)
+    optimizelyClientInstance.isFeatureEnabled(
+      'foo',
+      fooUserIdFalse,
+      fooCorrectSegmentationAttributes
+    )
   ).toEqual(false)
 
   expect(
-    optimizelyClientInstance.activate('foo', fooUserIdTrue, attributes)
+    optimizelyClientInstance.activate(
+      'foo',
+      fooUserIdTrue,
+      fooCorrectSegmentationAttributes
+    )
   ).toEqual('b')
 
   expect(
-    optimizelyClientInstance.activate('foo', fooUserIdFalse, attributes)
+    optimizelyClientInstance.activate(
+      'foo',
+      fooUserIdFalse,
+      fooCorrectSegmentationAttributes
+    )
   ).toEqual('a')
 
-  /*
   expect(
     optimizelyClientInstance.activate(
       'nonexistent',
-      'variation',
       fooUserIdFalse,
-      attributes
+      fooCorrectSegmentationAttributes
     )
   ).toEqual(null)
-  */
-
-  /*
-  expect(
-    optimizelyClientInstance.activate(
-      'foo',
-      'variation',
-      fooUserIdFalse,
-      attributes
-    )
-  ).toEqual(null)
-  */
 
   mockEventListener.mockClear()
 
   expect(
-    optimizelyClientInstance.activate('foo', fooUserIdTrue, attributes)
+    optimizelyClientInstance.activate(
+      'foo',
+      fooUserIdTrue,
+      fooCorrectSegmentationAttributes
+    )
   ).toEqual('b')
 
   expect(
-    optimizelyClientInstance.activate('foo', fooUserIdFalse, attributes)
+    optimizelyClientInstance.activate(
+      'foo',
+      fooUserIdFalse,
+      fooCorrectSegmentationAttributes
+    )
   ).toEqual('a')
 
   expect(mockEventListener).toHaveBeenCalledTimes(2)
@@ -96,34 +115,46 @@ it('returns the correct result experiments / activate', () => {
   // Feature flags
   mockEventListener.mockClear()
   expect(
-    optimizelyClientInstance.isFeatureEnabled('bar', 'w=zkhkjz')
+    optimizelyClientInstance.isFeatureEnabled('bar', barUserIdFalse)
   ).toBeFalsy()
-  // console.log(mockEventListener.mock.calls[0])
   expect(mockEventListener).toHaveBeenCalledTimes(1)
 
   expect(
-    optimizelyClientInstance.isFeatureEnabled('bar', '2987010978kjhkjhd82')
+    optimizelyClientInstance.isFeatureEnabled('bar', barUserIdTrue)
   ).toBeTruthy()
   // console.log(mockEventListener.mock.calls[1])
 
   expect(mockEventListener).toHaveBeenCalledTimes(2)
 
   mockEventListener.mockClear()
-  expect(optimizelyClientInstance.activate('bar', 'w=zkhkjz')).toEqual('a')
+  expect(optimizelyClientInstance.activate('bar', barUserIdFalse)).toEqual('a')
   expect(mockEventListener).toHaveBeenCalledTimes(1)
 
-  expect(
-    optimizelyClientInstance.activate('bar', '2987010978kjhkjhd82')
-  ).toEqual('b')
+  expect(optimizelyClientInstance.activate('bar', barUserIdTrue)).toEqual('b')
   expect(mockEventListener).toHaveBeenCalledTimes(2)
+})
 
-  /*
+it("doesn't call activate for experiments which your not eligble for based on audiences", () => {
+  mockEventListener.mockClear()
+  expect(optimizelyClientInstance.activate('foo', fooUserIdTrue)).toEqual(null)
+  expect(optimizelyClientInstance.activate('foo', fooUserIdFalse)).toEqual(null)
+  expect(mockEventListener).toHaveBeenCalledTimes(0)
+
+  mockEventListener.mockClear()
   expect(
-    optimizelyClientInstance.activate('bar', fooUserIdTrue, attributes)
-  ).toEqual('b')
+    optimizelyClientInstance.activate(
+      'foo',
+      fooUserIdTrue,
+      fooIncorrectSegmentationAttributes
+    )
+  ).toEqual(null)
 
   expect(
-    optimizelyClientInstance.activate('bar', fooUserIdTrue, attributes)
-  ).toEqual('a')
-  */
+    optimizelyClientInstance.activate(
+      'foo',
+      fooUserIdFalse,
+      fooIncorrectSegmentationAttributes
+    )
+  ).toEqual(null)
+  expect(mockEventListener).toHaveBeenCalledTimes(0)
 })
