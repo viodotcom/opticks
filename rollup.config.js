70b1922cfb1babb86aae01eaf0e22dc11a6a9270
Luis Felipe <luisfmsouza@gmail.com>
2020-09-11 09:25:30 +0200

    Clean up the code and dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,20 @@
-import babel from '@rollup/plugin-babel'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import {terser} from 'rollup-plugin-terser'
 import copy from 'rollup-plugin-copy'
 import typescript from '@rollup/plugin-typescript'
-
-const plugins = (outputDir) => [
-  // babel({
-  //   exclude: 'node_modules/**',
-  //   extensions: ['.ts'],
-  //   babelHelpers: 'bundled'
-  // }),
-  // resolve(),
-  // typescript({module: 'CommonJS'}),
-  typescript(),
-  // commonjs({extensions: ['.js', '.ts']}),
-  // terser(),
-  // TODO: doesn't seem to be working
-  copy({
-    'src/transform': `${outputDir}/transform`
-  })
-]
 
 const generateConfig = (integration) => ({
   input: `src/integrations/${integration}.ts`,
   output: {
     dir: 'lib',
-    // format: 'cjs',
     format: 'es',
     exports: 'named',
     sourcemap: true
   },
-  plugins: plugins('lib'),
+  plugins: [
+    typescript(),
+    copy({
+      'src/transform': `lib/transform`
+    })
+  ],
   external: [
     '@optimizely/optimizely-sdk',
     '@optimizely/optimizely-sdk/lib/utils/enums'
