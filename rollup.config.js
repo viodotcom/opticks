f30e7b6edb25e3bf6ea824c101e7384bbb7ba73c
Dale French <dfrench@findhotel.net>
2022-08-04 18:09:49 +0200

    update rollup config to minify and copy

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,59 @@
 import dts from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
+import copy from 'rollup-plugin-copy'
+import {terser} from 'rollup-plugin-terser'
 
-const generateConfig = (integrationName) => {
-  const bundle = (config) => ({
-    ...config,
-    input: `src/${integrationName}.ts`,
-    external: (id) => !/^[./]/.test(id),
-  })
+const OUTPUT_DIR = 'lib'
 
-  return (
-    bundle({
-      plugins: [esbuild()],
-      output: [
+const bundle = (config, integrationName) => ({
+  ...config,
+  input: `src/${integrationName}.ts`,
+  external: (id) => !/^[./]/.test(id),
+})
+
+const generateConfig = () => {
+  const libs = ['optimizely', 'simple']
+  const config = []
+  for (const lib of libs) {
+    config.push(
+      bundle(
         {
-          file: `${integrationName}.js`,
-          format: 'cjs',
-          sourcemap: true,
+          plugins: [
+            esbuild(),
+            copy({
+              'src/transform': `${OUTPUT_DIR}/transform`,
+            }),
+            terser(),
+          ],
+          output: [
+            {
+              file: `${OUTPUT_DIR}/${lib}.js`,
+              format: 'cjs',
+              sourcemap: true,
+            },
+            {
+              file: `${OUTPUT_DIR}/${lib}.mjs`,
+              format: 'es',
+              sourcemap: true,
+            },
+          ],
         },
+        lib,
+      ),
+      bundle(
         {
-          file: `${integrationName}.mjs`,
-          format: 'es',
-          sourcemap: true,
+          plugins: [dts()],
+          output: {
+            file: `${OUTPUT_DIR}/${lib}.d.ts`,
+            format: 'es',
+          },
         },
-      ],
-    }),
-    bundle({
-      plugins: [dts()],
-      output: {
-        file: `${integrationName}.d.ts`,
-        format: 'es',
-      },
-    })
-  )
+        lib,
+      ),
+    )
+  }
+
+  return config
 }
 
-const config = [generateConfig('optimizely'), generateConfig('simple')]
-
-export default config
+export default generateConfig()
