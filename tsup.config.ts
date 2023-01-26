21e0189714637383179ecc84a1d3b6f0264b997a
Dale French <dfrench@findhotel.net>
2023-01-26 12:41:51 +0100

    bundle transforms

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/simple.ts", "src/optimizely.ts"],
+  entry: [
+    "src/simple.ts",
+    "src/optimizely.ts",
+    "src/transform/toggle.ts",
+    "src/transform/booleanToggle.ts",
+  ],
   format: ["cjs", "esm"],
   dts: true,
   sourcemap: true,
