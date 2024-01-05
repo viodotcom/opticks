e1d436e191b3cdf20eb22d0181ebf298feab0714
Dale French <dfrench@findhotel.net>
2024-01-05 17:31:49 +0100

    update deps

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,25 +1,3 @@
 #!/usr/bin/env node
-import meow from 'meow';
 
-const cli = meow(`
-	Usage
-	  $ unicorn-fun [input]
-
-	Options
-	  --postfix  Lorem ipsum  [Default: rainbows]
-
-	Examples
-	  $ cli-name
-	  unicorns & rainbows
-	  $ cli-name ponies
-	  ponies & rainbows
-`, {
-	flags: {
-		postfix: {
-			type: 'string',
-			default: 'rainbows'
-		}
-	}
-});
-
-console.log(moduleName(cli.input[0] || 'unicorns', cli.flags));
+console.log('Hello World from CLI')
