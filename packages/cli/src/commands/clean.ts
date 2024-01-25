a67355c5f8b4f1904564111828085c6671865640
Dale French <dfrench@findhotel.net>
2024-01-25 16:30:07 +0100

    add initial transform script

--- packages/cli/src/commands/clean.ts
+++ packages/cli/src/commands/clean.ts
@@ -1,13 +1,29 @@
 import {readPackage} from 'read-pkg'
+import {exec} from 'child_process'
 
 export async function clean(argv) {
+  const {id, winner} = argv
+
   try {
     const packageJson = await readPackage()
     if (
       packageJson.dependencies &&
       Object.keys(packageJson.dependencies).includes('opticks')
     ) {
-      console.log('opticks installed')
+      const isWinner = winner === 'a' ? false : true
+
+      const cmd = `./node_modules/.bin/jscodeshift --transform ./node_modules/opticks-cli/dist/transform/toggle.mjs --parser=tsx --extensions=ts,tsx src --toggle=${id} --winner='${isWinner}'`
+
+      console.log(cmd)
+
+      exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error executing jscodeshift command: ${error}`)
+          return
+        }
+        console.log(stdout)
+        console.error(stderr)
+      })
     } else {
       console.log(
         `Could not find a dependency named 'opticks' installed in your project. Please install opticks and try again.`
--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/transform/*'],
   outDir: 'dist',
   format: ['esm'],
   dts: false,
